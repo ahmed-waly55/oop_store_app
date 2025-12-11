24022032c7f8bd5cd0b7d41bdf8c292949bf4eaf
ahmed waly <ahmedwaly074@gmail.com>
2025-12-12 01:25:17 +0200

    feat(Customer): implement permissions and cart handling

--- src/models/Customer.ts
+++ src/models/Customer.ts
@@ -1,7 +1,17 @@
 import { CustomerPermission, userType } from "../types/user";
+import { Cart } from "./Cart";
+import { Order } from "./Order";
 import { User } from "./User";
 
 class Customer extends User {
+    private readonly cart: Cart;
+    private readonly orders: Order[] = [];
+
+    constructor(name: string, email: string) {
+        super(name, email);
+        this.cart = new Cart();
+    }
+
     getRole(): userType {
         return 'Customer';
     }
