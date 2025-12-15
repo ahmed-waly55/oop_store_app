a778183f189c8fa907da40e3574bacf91fa365cb
ahmed waly <ahmedwaly074@gmail.com>
2025-12-15 13:41:05 +0200

    fix(cart): provide cart utilities and method stubs

--- src/models/Cart.ts
+++ src/models/Cart.ts
@@ -1,9 +1,36 @@
 import { CartItem } from "./CartItem";
+import { Product } from "./Product";
 
 export class Cart {
     private items: CartItem[] = [];
-    addItem() { }
-    removeItem() { }
+    addItem(product: Product, quantity: number): boolean {
+        const existingItems = this.items.find(item => item.product.id === product.id)
+        if (existingItems) {
+            const totalQuantity = existingItems.quantity + quantity;
+            // return true;
+            if (totalQuantity <= product.stock) {
+                existingItems.quantity += quantity;
+                return true;
+            }
+        } else {
+            if (quantity <= product.stock) {
+                this.items.push(new CartItem(product, quantity));
+                return true;
+            }
+
+        }
+
+        return false;
+
+    }
+    removeItem(productId: number): boolean {
+        const index = this.items.findIndex(item => item.product.id === productId);
+        if (index > -1) {
+            this.items.splice(index, 1);
+            return true
+        }
+        return false;
+    }
     getItems(): CartItem[] {
         return this.items;
     }
